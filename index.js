2319dab58f936b6593555c597ba13b3859d2b313
pj <pjhg14@gmail.com>
2021-03-23 19:13:14 -0400

    commit tree b8fb2f4297d1005f183b28ebe48fa05064ac44ca

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    let body = document.querySelector("body")
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+        .then(res => res.json())
+        .then(newUser => {
+            // let p = document.createElement("p")
+            // p.innerText = newUser.id
+
+            body.append(newUser.id)
+        }).catch(err => body.append(err.message))
+}
